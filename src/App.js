38d60462c4710cbc7f553c81187614c43e449b8a
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-12-14 14:56:53 +1300

    install test libs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import "react-native-gesture-handler";
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
-import { decode, encode } from "base-64";
-if (!global.btoa) {
-	global.btoa = encode;
-}
-if (!global.atob) {
-	global.atob = decode;
-}
-
-const Stack = createStackNavigator();
-
+import 'react-native-gesture-handler'
+import React, { useEffect, useState } from 'react'
+import { Text } from 'react-native'
+import { API_KEY, AUTH_DOMAIN, P_ID, S_BUCKET, M_SENDER_ID, APP_ID } from '@env'
 export default function App() {
-	const [loading, setLoading] = useState(true);
-	const [user, setUser] = useState(null);
-
-	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				{user ? (
-					<Stack.Screen name="Home">
-						{(props) => <HomeScreen {...props} extraData={user} />}
-					</Stack.Screen>
-				) : (
-					<>
-						<Stack.Screen name="Login" component={LoginScreen} />
-						<Stack.Screen name="Registration" component={RegistrationScreen} />
-					</>
-				)}
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return <Text>Hi!mbasdfjklsdbfjkasdbfjklasdBFjksdbfjklsdbgsdjkgbsdjkGbsdGJKb</Text>
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
+import { API_KEY, AUTH_DOMAIN, P_ID, S_BUCKET, M_SENDER_ID, APP_ID } from '@env'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBewvgedfA36q_7Lq4k8DG5bANoJ1g2BWM',
-  authDomain: 'meals4u-577ed.firebaseapp.com',
-  projectId: 'meals4u-577ed',
-  storageBucket: 'meals4u-577ed.appspot.com',
-  messagingSenderId: '1068900008919',
-  appId: '1:1068900008919:web:25983445bdc1d4a39a4192'
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: P_ID,
+  storageBucket: S_BUCKET,
+  messagingSenderId: M_SENDER_ID,
+  appId: APP_ID
 }
 
+console.log(firebaseConfig)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
